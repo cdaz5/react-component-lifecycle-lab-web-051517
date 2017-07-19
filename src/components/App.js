ef07e4a7f59831d8ac49ecb96a04b1e8ff40816f
github username <github email address>
2017-07-19 13:59:00 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,26 @@ class App extends React.Component {
 
   constructor() {
     super();
-    
+
     this.state = {
       latestTweets: []
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: componentDidMount()
-  // TODO: componentWillUnmount()
+//Before Being Placed
+  componentWillMount = () => {
+    this.fetchTweets()
+  }
+
+  //After it is Displayed
+  componentDidMount = () => {
+    this.startInterval()
+  }
+
+  //When it gets Deleted Clean Shit Up
+  componentWillUnmount = () => {
+    this.cleanUpInterval()
+  }
 
   startInterval = () => {
     this.interval = setInterval(this.fetchTweets, 2000);
--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -4,15 +4,31 @@ import Tweet from './Tweet';
 class TweetWall extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       tweets: []
     };
   }
 
-  // TODO: componentWillMount()
+  componentWillMount = (props) => {
+    this.setState({
+      tweets: [...this.state.tweets, ...this.props.newTweets]
+    })
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    // debugger
+    this.setState({
+      tweets: [...nextProps.newTweets, ...this.state.tweets]
+    })
+  }
+
+  shouldComponentUpdate = (nextProps) => {
+    // debugger
+    return (nextProps.newTweets.length > 0)
+  }
+
   // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
